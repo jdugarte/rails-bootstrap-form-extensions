b3ec63d7f5d43bd8bca81d3c8284a2a2a6d64834
Jesús Dugarte <jdugarte@gmail.com>
2015-09-22 16:46:14 -0400

    make the gem an engine, to make the assets in app available

--- lib/bootstrap_form_extensions.rb
+++ lib/bootstrap_form_extensions.rb
@@ -2,6 +2,10 @@ require 'bootstrap_form/form_builder'
 require 'bootstrap_form_extensions/submit_bar'
 
 module BootstrapFormExtensions
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
 end
 
 BootstrapForm::FormBuilder.send :include, BootstrapFormExtensions::SubmitBar
