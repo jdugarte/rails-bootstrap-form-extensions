8c0d57477f3f086191774ecc25e474fc294463e2
Jesús Dugarte <jdugarte@gmail.com>
2015-09-28 11:40:53 -0400

    namespace event as data-api, to comply with bootstrap behaviour

--- app/assets/javascripts/bootstrap_form_extensions/timespan.js
+++ app/assets/javascripts/bootstrap_form_extensions/timespan.js
@@ -6,7 +6,7 @@
 
   var Timespan = function (element) {
     this.$container = $(element)
-    this.$container.on('change', '.timespan-quantity, .timespan-unit', this.updateSeconds)
+    this.$container.on('change.bsfe.timespan.data-api', '.timespan-quantity, .timespan-unit', this.updateSeconds)
   }
 
   Timespan.VERSION = '0.0.3'
